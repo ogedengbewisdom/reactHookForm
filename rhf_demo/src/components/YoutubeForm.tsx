6256e821b515e737ae871a67cf470fc444159acc
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-01 18:44:19 +0100

    useform and devtools usage

--- rhf_demo/src/components/YoutubeForm.tsx
+++ rhf_demo/src/components/YoutubeForm.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools"
+import { Fragment } from "react";
 
 export const YoutubeForm = () => {
+  const form = useForm();
+  const {register, control} = form;
+
   return (
-    <form>
-        <label htmlFor='username'>Username</label>
-        <input type='text' name='username' id='username'/>
+    <Fragment>
+      <form>
+          <label htmlFor='username'>Username</label>
+          <input type='text' id='username' {...register("username")}/>
 
-        <label htmlFor='email'>Email</label>
-        <input type='text' name='email' id='email'/>
+          <label htmlFor='email'>Email</label>
+          <input type='text' id='email' {...register("email")} />
 
-        <label htmlFor='channel'>Channel</label>
-        <input type='text' name='channel' id='channel'/>
+          <label htmlFor='channel'>Channel</label>
+          <input type='text' id='channel' {...register("channel")}/>
 
-        <button>Submit</button>
-    </form>
+          
+          <button>Submit</button>
+      </form>
+      <DevTool control={control} />
+    </Fragment>
   )
 }
