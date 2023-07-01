44f66d36a24277afe3a82c4184b235c306281bb3
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-01 03:16:26 +0100

    root app

---
+++ rhf_demo/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { YoutubeForm } from './components/YoutubeForm'
+
+function App() {
+
+  return (
+    <>
+       <YoutubeForm/>
+    </>
+  )
+}
+
+export default App
