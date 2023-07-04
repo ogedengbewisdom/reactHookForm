08414aedaa18d9d1e6f3930427e9c6dcb94d4fc3
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-04 16:03:27 +0100

    adding dynamic form value

--- rhf_demo/src/components/YoutubeForm.tsx
+++ rhf_demo/src/components/YoutubeForm.tsx
@@ -3,29 +3,57 @@ import { DevTool } from "@hookform/devtools"
 import { Fragment } from "react";
 
 type FormValues = {
-  username: string,
-  email: string,
-  channel: string
-
+  username: string;
+  email: string;
+  channel: string;
+  social: {
+    twitter: string;
+    facebook: string;
+  };
+  phoneNumbers: string[];
+  phNumbers: [
+    {numbers: string}
+  ]
 }
 
 let rerender = 0;
 export const YoutubeForm = () => {
-  const form = useForm<FormValues>();
+  const form = useForm<FormValues>({
+    defaultValues: {
+      username: "Rash",
+      email: "",
+      channel: "",
+      social: {
+        twitter: "",
+        facebook: ""
+      },
+      phoneNumbers: ["", ""],
+      phNumbers: [{numbers: ""}]
+    }
+  });
+
+  // const form = useForm<FormValues>({
+  //   defaultValues: async () => {
+  //     const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  //     const data = await response.json();
+  //     return {
+  //       username: data.name,
+  //       email: data.email,
+  //       channel: data.website
+  //     }
+  //   }
+  // });
   const {register, control, handleSubmit, formState} = form;
 
-  const {errors, touchedFields} = formState
+  const { errors } = formState
 
-
-  const controlInput = touchedFields.username ? "errorInput" : "input"
-  
 
   // useEffect(() => {
   //   console.log("CHEKING RERENDERING!")
   // }, [])
 
   const submitHandler = (data: FormValues) => {
-    console.log("submitted", data)
+    console.log(data.phoneNumbers[1])
   }
   rerender++
   
@@ -35,19 +63,19 @@ export const YoutubeForm = () => {
       <form onSubmit={handleSubmit(submitHandler)} noValidate>
         <div className='form-control'>
           <label htmlFor='username'>Username</label>
-          <input className={controlInput} type='text' id='username' {...register("username", {
+          <input type='text' id='username' {...register("username", {
             required: {
             value: true,
             message: "Enter a valid name"
           },
-          validate: (fieldValue) => fieldValue.trim().length > 5 
+          validate: (fieldValue) => fieldValue.trim().length > 5 || "name must be greater than 5"
           })}/>
           {errors &&<p className="error">{errors.username?.message}</p>}
         </div>
 
         <div className="form-control">
           <label htmlFor='email'>Email</label>
-          <input className="input" type='text' id='email' {...register("email", {
+          <input type='text' id='email' {...register("email", {
             pattern: {
               value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
               message: "Invalid email format"
@@ -62,8 +90,32 @@ export const YoutubeForm = () => {
 
         <div className="form-control">
           <label htmlFor='channel'>Channel</label>
-          <input className="input" type='text' id='channel' {...register("channel", {required: "Required channel"})}/>
+          <input type='text' id='channel' {...register("channel", {required: "Required channel"})}/>
           {errors &&<p className="error">{errors.channel?.message}</p>}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor='twitter'>Twitter</label>
+          <input type='text' id='twitter' {...register("social.twitter", {required: "Required twitter"})}/>
+          {errors &&<p className="error">{errors.social?.twitter?.message}</p>}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor='facebook'>Facebook</label>
+          <input type='text' id='facebook' {...register("social.facebook", {required: "Required facebook"})}/>
+          {errors &&<p className="error">{errors.social?.facebook?.message}</p>}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor='primary-number'>Phone 1</label>
+          <input type='text' id='primary-number' {...register("phoneNumbers.0", {required: "Required primary-number"})}/>
+          {errors &&<p className="error">{errors.phoneNumbers?.[0]?.message}</p>}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor='secondary-number'>Phone 2</label>
+          <input type='text' id='secondary-number' {...register("phoneNumbers.1", {required: "Required secondary-number"})}/>
+          {errors &&<p className="error">{errors.phoneNumbers?.[1]?.message}</p>}
         </div>
           <button>Submit</button>
       </form>
